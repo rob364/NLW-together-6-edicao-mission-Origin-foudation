bae5e81b67682268bef3b5f8c7f96eda03d67ee9
Roberta Lacerda <robertalacerda@aix.com.br>
2022-05-27 14:56:48 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-const nav = document.querySelector('#header nav')
-
-const toggle = document.querySelectorAll("nav .toggle")
+/*abre e fecha o menu quando clicar no icone: hambueguer e x  &*/
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
- 
+
+/*Quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/*mudar o header da pagina quando der scrollbar */
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    //menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
