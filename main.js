0c601730e717b63a9d57020cfe9d5fd621d084ec
Roberta <vcindustria@gmail.com>
2022-05-31 00:28:20 -0300

    atualização

--- main.js
+++ main.js
@@ -1,44 +1,60 @@
-/*abre e fecha o menu quando clicar no icone: hambueguer e x  &*/
-const nav = document.querySelector("#header nav");
-const toggle = document.querySelectorAll("nav .toggle");
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener("click", function () {
-    nav.classList.toggle("show");
-  });
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
-/*Quando clicar em um item do menu, esconder o menu*/
-const links = document.querySelectorAll("nav ul li a");
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener("click", function () {
-    nav.classList.remove("show");
-  });
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
 
-/*mudar o header da pagina quando der scrollbar */
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
-    header.classList.add("scroll");
+    header.classList.add('scroll')
   } else {
-    //menor que a altura do header
-    header.classList.remove("scroll");
+    // menor que a altura do header
+    header.classList.remove('scroll')
   }
-});
+})
 
-/*Testimonials corousel slider swiper */
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination'  
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
 
-  /* scrollreveal */
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
