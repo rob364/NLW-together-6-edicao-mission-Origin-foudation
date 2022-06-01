3ac5a028f8ee518b3ad49766de3061edb76a14d1
Roberta Lacerda <robertalacerda@aix.com.br>
2022-06-01 13:46:38 -0300

    atualização

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*abre e fecha o menu quando clicar no icone: hambueguer e x  &*/
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   });
 }
 
-/*Quando clicar em um item do menu, esconder o menu*/
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
@@ -17,28 +17,84 @@ for (const link of links) {
   });
 }
 
-/*mudar o header da pagina quando der scrollbar */
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+/* mudar o header da página quando der scroll */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add("scroll");
   } else {
-    //menor que a altura do header
+    // menor que a altura do header
     header.classList.remove("scroll");
   }
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
 
-/*Testimonials corousel slider swiper */
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination'  
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
-  /* scrollreveal */
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
+
+/* Botão voltar para o topo*/
+
+function bacToTop() {
+  const backToTopButtom = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+/* When Scroll*/
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
+
+/*
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+*/
+
+/* When Scroll 
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
+
+*/
